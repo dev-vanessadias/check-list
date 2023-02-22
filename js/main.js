618ed7315148f2be34c09f000943297cac877d7b
dev-vanessadias <dev.vanessadias@gmail.com>
2023-02-22 10:42:01 -0300

    JS todoList

--- js/main.js
+++ js/main.js
@@ -17,7 +17,28 @@ const saveTodo = (text) => {
     todoTitle.innerText = text;
     todo.appendChild(todoTitle);
 
-}
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<img id="icone-adicionar" src="/img/addicon.png" />';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<img id="icone-editar"  src="./img/editicon.png">';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<img id="icone-deletar" src="./img/deleteicon.png">';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    todoInput.focus();
+
+
+};
 
 //Eventos
 todoForm.addEventListener("submit", (e) => {
